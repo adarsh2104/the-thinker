bf860916fc1fa0cf990b325d1c34c4452a6d92f3
Adarsh Sethi <adarsh.2104@gmail.com>
2021-03-21 23:11:47 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import WbIncandescentTwoToneIcon from '@material-ui/icons/WbIncandescentTwoTone';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <WbIncandescentTwoToneIcon fontSize='large'/>
+        &nbsp;Thought Galaxy !!
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
